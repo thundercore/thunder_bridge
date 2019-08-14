29e1c74635bed0bc49f4d3fb32e0b91dd1e0f9ce
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-08-14 13:57:08 +0800

    add in tutorial menu

--- ui/src/components/MobileMenu.js
+++ ui/src/components/MobileMenu.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { MenuItems } from './MenuItems'
+import {TutorialIcon} from "./menu-icons"
 
 export const MobileMenu = ({ onMenuToggle, withoutEvents }) => (
   <div className="mobile-menu">
     <div className="mobile-menu-links">
       <MenuItems withoutEvents={withoutEvents} onMenuToggle={onMenuToggle} />
+      <a href="https://thunder-docs.s3-us-west-2.amazonaws.com/ThunderStableCoinTutorial.pdf"
+         target="_blank"
+         className="menu-items" onClick={withoutEvents.onMenuToggle}>
+        <span className="menu-items-icon"><TutorialIcon/></span>
+        <span className="menu-items-text">Tutorial</span>
+      </a>
     </div>
   </div>
 )
