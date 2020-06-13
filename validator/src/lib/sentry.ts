67e9154f588efeb9ff1c0367b4fa9773121d3b45
kcfang <kevin@thundertoken.tw>
2020-06-13 17:39:18 +0800

    Add more sentry information

--- validator/src/lib/sentry.ts
+++ validator/src/lib/sentry.ts
@@ -14,24 +14,22 @@ declare global {
 global.__rootdir__ = __dirname || process.cwd();
 
 export default function init() {
-  let hostname;
-  if (config.VALIDATOR_TAG) {
-    hostname = `${config.name}-${config.VALIDATOR_TAG}`
-  } else {
-    hostname = os.hostname()
-  }
-
   Sentry.init({
-    serverName: hostname,
+    serverName: os.hostname(),
     integrations: [new RewriteFrames({
       root: global.__rootdir__
     })]
   });
 
   Sentry.configureScope(function(scope) {
+  if (config.VALIDATOR_TAG) {
+    scope.setTag('service', `${config.name}-${config.VALIDATOR_TAG}`)
+  } else {
     scope.setTag("service", config.name)
-    scope.setTag("web3Host", config.web3.currentProvider.urls.toString())
+  }
+
+  scope.setTag("web3Host", config.web3.currentProvider.urls.toString())
   })
 
-  Sentry.captureMessage('Init service', Sentry.Severity.Debug)
+  Sentry.captureMessage(`Init ${config.SENTRY_ENVIRONMENT} validator`, Sentry.Severity.Debug)
 }
