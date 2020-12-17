e6882eec1b7698d8ae8542d17805c5dac0c10986
kcfang <kevin@thundertoken.tw>
2020-12-17 16:01:09 +0800

    Retire SAI

--- ui/src/components/utils/renameToken.js
+++ ui/src/components/utils/renameToken.js
@@ -1,8 +1,5 @@
 
 export const RenameToken = (name) => {
-  if (name.includes('DAI')) {
-    return name.replace('DAI', 'SAI')
-  }
   if (name.includes('TWETH')) {
     return name.replace('TWETH', 'ETH')
   }
