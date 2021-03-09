02887ead42e90a5a526e17fbabefc1902c171d0d
Peter <peter@thundercore.tw>
2021-03-09 13:01:56 +0800

    bsc tutorial

--- ui/src/components/HeaderMenu.js
+++ ui/src/components/HeaderMenu.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { MenuItems } from './MenuItems'
 import { Wallet } from './Wallet'
 import { TutorialIcon } from "./menu-icons"
+import { bridgeType } from '../stores/utils/bridgeMode'
 
 export const HeaderMenu = ({ withoutEvents }) => (
   <div className="header-menu">
     <MenuItems withoutEvents={withoutEvents} />
     <Wallet />
-    <a href="https://docs.thundercore.com/docs/ThunderStableCoinTutorial.pdf"
+    <a href={`${bridgeType === "eth" ? "https://docs.thundercore.com/docs/ThunderStableCoinTutorial.pdf" : "https://docs.thundercore.com/docs/TransferringCrossChainAssets-BSC.pdf" }`}
       target="_blank"
       className="menu-items" onClick={withoutEvents.onMenuToggle}>
       <span className="menu-items-icon"><TutorialIcon/></span>
--- ui/src/components/MobileMenu.js
+++ ui/src/components/MobileMenu.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { MenuItems } from './MenuItems'
 import {TutorialIcon} from "./menu-icons"
+import { bridgeType } from '../stores/utils/bridgeMode'
 
 export const MobileMenu = ({ onMenuToggle, withoutEvents }) => (
   <div className="mobile-menu">
     <div className="mobile-menu-links">
       <MenuItems withoutEvents={withoutEvents} onMenuToggle={onMenuToggle} />
-      <a href="https://docs.thundercore.com/docs/ThunderStableCoinTutorial.pdf"
+      <a href={`${bridgeType === "eth" ? "https://docs.thundercore.com/docs/ThunderStableCoinTutorial.pdf" : "https://docs.thundercore.com/docs/TransferringCrossChainAssets-BSC.pdf" }`}
          target="_blank"
          className="menu-items" onClick={withoutEvents.onMenuToggle}>
         <span className="menu-items-icon"><TutorialIcon/></span>
