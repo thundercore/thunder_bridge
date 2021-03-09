ac7646b8a6e88527adfe76b8bdeb6e96db4cd41a
Peter <peter@thundercore.tw>
2021-03-09 11:26:43 +0800

    fix switch button

--- ui/src/components/SwithChainButton.js
+++ ui/src/components/SwithChainButton.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {bridgeType} from "../stores/utils/bridgeMode"
+import {withRouter } from 'react-router-dom'
 
-export default class SwithChainButton extends Component {
+const SwithChainButton = (props) => {
+  const bridgeText = bridgeType === "eth" ? "Binance Smart Chain Network" : "Ethereum Network"
+  const bridge = bridgeType === "eth" ? "bsc" : "eth"
 
-  bridgeText = bridgeType === "eth" ? "Binance Smart Chain Network" : "Ethereum Network"
-
-  getUrl() {
-    const env = process.env.NODE_ENV
-    const bridge = process.env.REACT_APP_BRIDGE_TOKENS.toLowerCase().includes("eth") ? "bsc" : "eth"
-    if (env === "development") return `https://bridge-venus.thundercore.com/${bridge}`
-    return `https://bridge.thundercore.com/${bridge}`
-  }
-
-  render() {
-    return (
-      <div className="switch-chain-wrapper">
-        <a href={this.getUrl()}><button className="switch-chain-button" type="button">
-          <span className="to">Switch to</span>
-          <span className="text">{this.bridgeText}</span></button></a>
-      </div>
-    )
-  }
+  return (
+    <div className="switch-chain-wrapper">
+      <a href={`${window.origin}/${bridge}`}><button className="switch-chain-button" type="button">
+        <span className="to">Switch to</span>
+        <span className="text">{bridgeText}</span></button></a>
+    </div>
+  )
 }
+
+export default withRouter(SwithChainButton)
