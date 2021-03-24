8ef93fe264329a3c7552ba2b9bacf6c3c5486426
Peter <peter@thundercore.tw>
2021-03-24 18:34:54 +0800

    update switch network logic

--- ui/src/components/SweetAlert.js
+++ ui/src/components/SweetAlert.js
@@ -2,6 +2,7 @@ import React from 'react'
 import swal from 'sweetalert'
 import { inject, observer } from 'mobx-react'
 import Web3Store from '../stores/Web3Store'
+import { isMobile } from 'react-device-detect'
 
 @inject('RootStore')
 @observer
@@ -11,7 +12,7 @@ export class SweetAlert extends React.Component {
 
     if (alertStore.alerts.length > 0) {
       const alert = alertStore.alerts.slice()[0]
-      const isAddNetwork = alert.info.id !== 1 // check not eth network. For eth, check this PR: https://github.com/ethereum/EIPs/pull/3326
+      const isAddNetwork = window.ethereum && window.ethereum.isMetaMask && !isMobile && alert.info.id !== 1 // check not eth network. For eth, check this PR: https://github.com/ethereum/EIPs/pull/3326
       const button = isAddNetwork ? {button: {text: `Switch to ${alert.info.name}`}} : {}
       const swalConfig = {...alert, ...button}
       swal(swalConfig).then((isButtonClicked) => {
